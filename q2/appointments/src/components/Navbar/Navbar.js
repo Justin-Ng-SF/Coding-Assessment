e219b754c7ec4d3c44e656b16c6a21b1f66820ee
Justin Ng <justinng123409@yahoo.com>
2021-10-30 18:26:52 -0700

    removed links from navbar and made into new component

--- q2/appointments/src/components/Navbar/Navbar.js
+++ q2/appointments/src/components/Navbar/Navbar.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import nemsLogoIcon from '../../assets/nemsLogoIcon.png';
+import './Navbar.css'
 
 const Navbar = () => {
 
     return (
-        <div>
-            <h3>Serving the medically-underserved populations of the San Francisco Bay Area since 1971</h3>
-        
+        <div className="navbar">
 
-            <a href="/appointments">Current Appointments</a>
-            {` `}
-            <a href="/appointments/booking">Book an Appointment</a>
+
+            <div className="welcome-container">
+                <a href="/appointments">
+                    <img src={nemsLogoIcon} id="nemsLogoIcon" alt="" />
+                </a>
+                
+                <h3 id="welcome-header">Serving the medically-underserved populations of the San Francisco Bay Area since 1971.</h3>
+                
+            </div>
+            {/* <div className="links-container">
+                <div className="links-box">
+                    <a href="/appointments/booking" className="link">Book an Appointment</a>
+                    <a href="/appointments" className="link">Appointments</a>
+                    <a href="/openHours" className="link">Open Hours</a>
+                    <a href="/contact" className="link">Contact Us</a>
+            
+                </div>
+            </div> */}
             
         </div>
     )
