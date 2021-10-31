fb8573c87af304867e42883c3aa4d6f5a106e01f
Justin Ng <justinng123409@yahoo.com>
2021-10-30 18:27:57 -0700

    added new components

--- q2/appointments/src/App.js
+++ q2/appointments/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Appointments from './components/Appointment/Appointments';
 import Confirmed from './components/Booking/Confirmed';
 import Booking from './components/Booking/Booking';
+import LinksBar from './components/LinksBar/LinksBar';
 
 
 
@@ -13,7 +14,7 @@ function App() {
   //will be using time as the key as we will assume that there cannot be two appointments at the same time, meaning that times 'should' be unique in this scenario
 
   const appointments = [
-    { "time": "2021-10-30T13:00:00Z", "provider": "Dr. Doe" },
+    { "time": "2021-10-01T13:00:00Z", "provider": "Dr. Doe" },
     { "time": "2021-10-01T16:00:00Z", "provider": "Dr. David" },
     { "time": "2021-10-01T13:30:00Z", "provider": "Dr. Doe" },
     { "time": "2021-10-02T15:40:00Z", "provider": "Dr. David" },
@@ -23,7 +24,7 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        
+        <LinksBar />
         <Switch>
           <Route exact path="/appointments" component={() => <Appointments appointments={appointments} />} />
           <Route exact path="/appointments/booking" component={() => <Booking appointments={appointments} />} />
